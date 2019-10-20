42fd17ec6e3bbe54f9d8839c1696855560b1e320
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 14:05:44 +0700

    RefactoringTowardsALoosely-coupledDesign

---
+++ TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using _5_3_RefactoringTowardsALoosely_coupledDesign.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
